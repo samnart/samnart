375ce0ea82b6a5647f35612d735eeba5cd6cae01
samnart <tettey1234567@gmail.com>
2024-03-27 23:17:34 +0100

    made some tiny changes

---
+++ js-projects/load-bar/simple-http.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Wowwzy');
+    } else if (req.url === '/about') {
+        res.end('about us');
+    } else {
+        res.end(`
+            <div>Wrong Page Here</div>
+        `)
+    }
+})
+
+server.listen(1500);
